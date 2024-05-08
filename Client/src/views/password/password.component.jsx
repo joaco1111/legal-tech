2f62868cd13843c5dd8954d1cca2ed26c945cd7c
melizulian <melizulian@hotmail.com>
2024-05-08 15:32:43 -0300

    views, consultations and customers

---
+++ Client/src/views/password/password.component.jsx
@@ -0,0 +1,15 @@
+import './password.module.css';
+import React from 'react';
+
+function Password() {
+  return (
+    <div>
+      <div>
+        <p>Password</p>
+          
+      </div>
+    </div>
+  )
+}
+
+export default Password
