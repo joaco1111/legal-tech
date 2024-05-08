c8405ba7c5fc68c4daf11068de860d9b569b4142
Joaquin Ortega <joacoo973@gmail.com>
2024-05-07 21:21:29 -0300

    New branch

---
+++ Client/src/main.jsx
@@ -0,0 +1,20 @@
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <GoogleOAuthProvider clientId="526585059274-7hpu57211jdp1ivjc0bec7ba5jkpcdaa.apps.googleusercontent.com">
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </GoogleOAuthProvider>
+);
