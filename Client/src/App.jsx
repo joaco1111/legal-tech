efd1ad110a692e6514c0e4acb87b691c62f3955c
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-09 22:22:09 -0500

    rutas front

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,7 +8,7 @@ import Lawyers from './views/lawyers/lawyers.components';
 import Documents from './views/documents/documents.component';
 import Diary from './views/diary/diary.components';
 import Payments from './views/payments/payments.component';
-import Consult from './views/consultations/consultations.component';
+import Consultations from './views/consultations/consultations.component';
 import Statistics from './views/statistics/statistics.component';
 import CreateUser from './views/createUser/createUser.component'
 import Password from './views/password/password.component';
@@ -32,24 +32,84 @@ function App() {
         </div>
       ) : undefined} */}
       <Routes>
+        <Route path="/" element={<Landing />} />
 
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/crearusuario" element={<CreateUser/>}/>
-        <Route path="/recordatoriocontrasena" element={<Password/>}/>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/home/detail' element={<Detail/>}/>
-        <Route path='/home/cases/:id' element={<Cases/>}/>
-        <Route path='/home/costumers/:id' element={<Costumers/>}/>
-        <Route path='/home/lawyers/:id' element={<Lawyers/>}/>
-        <Route path='/home/documents/:id' element={<Documents/>}/>
-        <Route path='/home/diary' element={<Diary/>}/>
-        <Route path='/home/payments' element={<Payments/>}/>
-        <Route path='/home/consultation' element={<Consultations/>}/>
-        <Route path='/home/statistics' element={<Statistics/>}/>
+        <Route path="/password" element={<Password />} />
+        <Route path="/crearusuario" element={<CreateUser />} />
+        {/* <Route
+          path="/generarfactura"
+          element={isAuthenticated ? <GenerarFactura /> : <Landing />}
+        />
+        <Route
+          path="/invoice"
+          element={isAuthenticated ? <Invoice /> : <Landing />}
+        />
+        <Route
+          path="/authorization"
+          element={isAuthenticated ? <Authorization /> : <Landing />}
+        />
+        <Route
+          path="/poder"
+          element={isAuthenticated ? <Poder /> : <Landing />}
+        />
+        <Route
+          path="/bankruptcy"
+          element={isAuthenticated ? <Bankruptcy /> : <Landing />}
+        />
+        <Route
+          path="home/clientregister"
+          element={isAuthenticated ? <Client /> : <Landing />}
+        />
+        <Route
+          path="home/reminders"
+          element={isAuthenticated ? <ConfigReminders/> : <Landing />}
+        />
 
+        <Route
+          path="home/contract"
+          element={isAuthenticated ? <Contract /> : <Landing />}
+        /> */}
+
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home/detail"
+          element={isAuthenticated ? <Detail /> : <Landing />}
+        />
+        <Route
+          path="/home/cases/:id"
+          element={isAuthenticated ? <Cases /> : <Landing />}
+        />
+        <Route
+          path="/home/costumers/:id"
+          element={isAuthenticated ? <Costumers /> : <Landing />}
+        />
+        <Route
+          path="/home/lawyers/:id"
+          element={isAuthenticated ? <Lawyers /> : <Landing />}
+        />
+        <Route
+          path="/home/documents/:id"
+          element={isAuthenticated ? <Documents /> : <Landing />}
+        />
+        <Route
+          path="/home/diary"
+          element={isAuthenticated ? <Diary /> : <Landing />}
+        />
+        <Route
+          path="/home/payments"
+          element={isAuthenticated ? <Payments /> : <Landing />}
+        />
+        <Route
+          path="/home/consultations"
+          element={isAuthenticated ? <Consultations /> : <Landing />}
+        />
+        <Route
+          path="/home/statistics"
+          element={isAuthenticated ? <Statistics /> : <Landing />}
+        />
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
